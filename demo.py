826fe611ce97f71ee792b1623af5e1d3302b4a21
Excergic <dhaivat.jambudia@gmail.com>
2025-07-18 17:06:16 +0530

    mongodb-logger-exception complete

---
+++ demo.py
@@ -0,0 +1,15 @@
+# following code is to check the logging config
+
+# from src.logger import logging
+# logging.debug("This is a debug message")
+
+# exception handling
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# try:
+#     a = 1 + '2'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
